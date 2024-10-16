0c29f68ab79edb394aaf3a629114cad4b260b211
navneetthakor <tnavneet8628@gmail.com>
2024-10-16 09:41:25 +0530

    basic of array is completed

---
+++ 6_array/1_basic_of_array.js
@@ -0,0 +1,7 @@
+// two ways to declare array 
+var arr1 = [];
+var arr2 = [];
+// - both declarations are same 
+// - if we not give this data type and initialize empty array then defualt data type for array will be 'never'
+var arr3 = [1];
+// - if we are giving some initial values in array then it will figure out data-type by it self
---
+++ 6_array/1_basic_of_array.ts
@@ -0,0 +1,8 @@
+// two ways to declare array 
+const arr1: string[] = [];
+const arr2: Array<string> = [];
+    // - both declarations are same 
+    // - if we not give this data type and initialize empty array then defualt data type for array will be 'never'
+
+const arr3 = [1];
+    // - if we are giving some initial values in array then it will figure out data-type by it self
