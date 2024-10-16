9d4e91a9584333aa4d8fa8ea2fe70cf89d330c59
navneetthakor <tnavneet8628@gmail.com>
2024-10-16 22:44:05 +0530

    small modification in index.ts file of project setup

--- 11_project_setup_with_ts/src/index.ts
+++ 11_project_setup_with_ts/src/index.ts
@@ -1,3 +1,8 @@
 console.log("hello");
 
 console.log("Hello 2");
+
+// use below command for automatic compilation of all files to dist folder
+// tsc -w 
+// and make sure to change output directory in tsconfi.json
+// "outDir": "./dist",   
