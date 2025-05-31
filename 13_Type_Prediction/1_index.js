d1d32c2cd2e721399fc974ba62a5019bf9e28835
NAVNEETKUMAR RAMANBHAI THAKOR <tnavneet8628@gmail.com>
2025-05-31 10:35:19 +0000

    Typescript is completed (phase-1)

---
+++ 13_Type_Prediction/1_index.js
@@ -0,0 +1,9 @@
+/*
+sometimes we are working more than one type in a function or class, so at those moments need my be arrive to
+accurately decide which data type is it
+- for that follow below official documentation of typescript
+
+https://www.typescriptlang.org/docs/handbook/2/narrowing.html
+
+*/
+console.log("Type Prediction");
